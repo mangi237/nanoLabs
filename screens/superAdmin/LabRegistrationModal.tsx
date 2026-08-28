f51de3501b9f67268a97b062bbcaeba5d7f19f45
Mangi237 <mangilerinelasliejr@gmail.com>
2026-08-28 19:37:22 +0100

    hope

--- screens/superAdmin/LabRegistrationModal.tsx
+++ screens/superAdmin/LabRegistrationModal.tsx
@@ -34,7 +34,7 @@ import {
   MapPin
 } from 'lucide-react';
 import { collection, addDoc, db } from '../../services/firebase';
-import { uploadService } from '../../services/uploadService';
+import { uploadService } from '../../api/upload';
 import { sendOtpVerification, verifyOtpCode, sendLabWelcomeEmail } from '../../services/emailService';
 import LabTermsModal from '../../components/legal/LabTermsModal';
 import { PricingModelType, SubscriptionTierType } from '../../types';
